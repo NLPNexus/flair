54b5b78d33636bdaf88fdaa149071fcdcaa132a1
susannaruecker <susanna.ruecker@hu-berlin.de>
2024-10-16 12:15:43 +0200

    formatting

--- flair/datasets/sequence_labeling.py
+++ flair/datasets/sequence_labeling.py
@@ -3,14 +3,13 @@ import json
 import logging
 import os
 import re
+import shutil
+import tempfile
+import zipfile
 
 # import shutil
 from collections import defaultdict
 from pathlib import Path
-import tempfile
-import shutil
-import requests
-import zipfile
 from typing import (
     Any,
     DefaultDict,
@@ -24,6 +23,7 @@ from typing import (
     cast,
 )
 
+import requests
 from torch.utils.data import ConcatDataset, Dataset
 
 import flair
@@ -1512,7 +1512,7 @@ class CLEANCONLL(ColumnCorpus):
             return changes
 
         def parse_line_range(line_range_str):
-            """Utility function to parse a line range string like '17703,17705' or '5727' and returns a tuple (start, end)"""
+            """Utility function to parse a line range string like '17703,17705' or '5727' and returns a tuple (start, end)."""
             parts = line_range_str.split(",")
             if len(parts) == 1:
                 start = int(parts[0]) - 1
@@ -1523,9 +1523,7 @@ class CLEANCONLL(ColumnCorpus):
                 return (start, end)
 
         def apply_patch_to_file(original_file, changes, output_file_path):
-            """
-            Applies the patch instructions to the content of the original file.
-            """
+            """Applies the patch instructions to the content of the original file."""
             with open(original_file, encoding="utf-8") as f:
                 original_lines = f.readlines()
 
